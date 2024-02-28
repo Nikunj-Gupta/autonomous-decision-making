2ab688fcd796b4d1ba576ba9acc92ca5b577c00f
nikunjgupta <nikunjgupta@nikunjgupta.usc.edu>
2024-02-28 12:18:28 -0800

    seeds, maps, algos, [git status

--- code/runs.py
+++ code/runs.py
@@ -1,15 +1,15 @@
 import os 
 
 PARALLEL=True # False
-SEEDS = list(range(2)) 
+SEEDS = list(range(10)) 
 MAPS = ["easy_0", "easy_1", "medium_0", "medium_1", "hard_0", "hard_1"] 
 ALGOS = ["RandomAgent", "SARSALearner", "QLearner"] 
 EXPLORATION_STRATEGIES = ["random_bandit", "epsilon_greedy", "boltzmann","UCB1"] 
 
-for map in MAPS: 
+for m in MAPS: 
     for seed in SEEDS: 
         for algo in ALGOS: 
             for explore in EXPLORATION_STRATEGIES: 
-                if PARALLEL: os.system(f"python main.py --map {map} --algo {algo} --exploration_strategy {explore} --seed {seed} &") 
-                else: os.system(f"python main.py --map {map} --algo {algo} --exploration_strategy {explore} --seed {seed}") 
+                if PARALLEL: os.system(f"python main.py --map {m} --algo {algo} --exploration_strategy {explore} --seed {seed} &") 
+                else: os.system(f"python main.py --map {m} --algo {algo} --exploration_strategy {explore} --seed {seed}") 
 
