7719e44898ceff8b57cff6fcc05eaecee39ac69d
Nikunj-Gupta <ng2531@nyu.edu>
2024-02-28 11:50:44 -0800

    parallel toggle

--- code/runs.py
+++ code/runs.py
@@ -1,6 +1,6 @@
 import os 
 
-
+PARALLEL=True # False
 SEEDS = list(range(2)) 
 MAPS = ["easy_0", "easy_1", "medium_0", "medium_1", "hard_0", "hard_1"] 
 ALGOS = ["RandomAgent", "SARSALearner", "QLearner"] 
@@ -10,6 +10,6 @@ for map in MAPS:
     for seed in SEEDS: 
         for algo in ALGOS: 
             for explore in EXPLORATION_STRATEGIES: 
-                os.system(f"python main.py --map {map} --algo {algo} --exploration_strategy {explore} --seed {seed} &") 
-
+                if PARALLEL: os.system(f"python main.py --map {map} --algo {algo} --exploration_strategy {explore} --seed {seed} &") 
+                else: os.system(f"python main.py --map {map} --algo {algo} --exploration_strategy {explore} --seed {seed}") 
 
