c27ad1aada1bb2a9e759e9b2785c81e0e29e78e6
Nikunj-Gupta <ng2531@nyu.edu>
2024-02-28 11:29:48 -0800

    exploration_strategy runs

--- code/runs.py
+++ code/runs.py
@@ -1,10 +1,10 @@
 import os 
 
 
-SEEDS = list(range(5)) 
-# MAPS = ["easy_0", "easy_1"]
-MAPS = ["medium_0", "medium_1"]
+SEEDS = list(range(2)) 
+MAPS = ["easy_0", "easy_1", "medium_0", "medium_1", "hard_0", "hard_1"] 
 ALGOS = ["RandomAgent", "SARSALearner", "QLearner"] 
+EXPLORATION_STRATEGIES = ["random_bandit", "epsilon_greedy", "boltzmann","UCB1"] 
 
 for map in MAPS: 
     for seed in SEEDS: 
