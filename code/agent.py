1d36c545b998337eb6c4e20be3a36a46a259cdbb
Nikunj-Gupta <ng2531@nyu.edu>
2024-03-01 14:06:32 -0800

    save/load model

--- code/agent.py
+++ code/agent.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
 from multi_armed_bandits import *
+import pickle 
+import os 
 
 """
  Base class of an autonomously acting and learning agent.
@@ -48,6 +50,7 @@ class TemporalDifferenceLearningAgent(Agent):
         self.epsilon_decay = params["epsilon_decay"]
         self.epsilon = 1.0
         self.exploration_strategy = params["exploration_strategy"] 
+        self.exp_path = params["exp_path"] 
         self.action_counts = np.zeros(self.nr_actions) 
         
     def Q(self, state):
@@ -72,6 +75,21 @@ class TemporalDifferenceLearningAgent(Agent):
     
     def decay_exploration(self):
         self.epsilon = max(self.epsilon-self.epsilon_decay, self.epsilon_decay)
+    
+    def save_model(self, checkpoint=None): 
+        if checkpoint: 
+            save_path = os.path.join(self.exp_path, "model_checkpoints")
+            if not os.path.exists(save_path): os.makedirs(save_path) 
+            save_name = f"model_checkpoint_{checkpoint}.pkl" 
+        else: 
+            save_path = self.exp_path 
+            save_name = "model.pkl"         
+        with open(os.path.join(save_path, save_name), 'wb') as f:
+            pickle.dump(self.Q_values, f) 
+    
+    def load_model(self, load_path): 
+        with open(os.path.join(load_path), 'rb') as f: 
+            self.Q_values = pickle.load(f) 
 
 """
  Autonomous agent using on-policy SARSA.
