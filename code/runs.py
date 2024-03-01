6217261edf575f331f73ffe6651c17c2c223c759
nikunjgupta <nikunjgupta@nikunjgupta.usc.edu>
2024-03-01 14:25:34 -0800

    updated manual seed to run id

--- code/runs.py
+++ code/runs.py
@@ -1,15 +1,15 @@
 import os 
 
 PARALLEL=True # False
-SEEDS = list(range(10)) 
+RUNS = list(range(1, 21)) 
 MAPS = ["easy_0", "easy_1", "medium_0", "medium_1", "hard_0", "hard_1"] 
 ALGOS = ["RandomAgent", "SARSALearner", "QLearner"] 
 EXPLORATION_STRATEGIES = ["random_bandit", "epsilon_greedy", "boltzmann","UCB1"] 
 
 for m in MAPS: 
-    for seed in SEEDS: 
+    for run_id in RUNS: 
         for algo in ALGOS: 
             for explore in EXPLORATION_STRATEGIES: 
-                if PARALLEL: os.system(f"python main.py --map {m} --algo {algo} --exploration_strategy {explore} --seed {seed} &") 
-                else: os.system(f"python main.py --map {m} --algo {algo} --exploration_strategy {explore} --seed {seed}") 
+                if PARALLEL: os.system(f"python main.py --map {m} --algo {algo} --exploration_strategy {explore} --run_id {run_id} &") 
+                else: os.system(f"python main.py --map {m} --algo {algo} --exploration_strategy {explore} --run_id {run_id}") 
 
